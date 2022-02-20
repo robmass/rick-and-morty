592f5fcd1b438d5a02eee2a28927796aca12096f
robmass <robertomassimini@hotmail.com>
2022-02-20 16:42:03 +0100

    feat: character grid component

---
+++ src/components/characters-grid/index.tsx
@@ -0,0 +1,21 @@
+import { CharacterCard } from "@/components/character-card";
+import "@/components/characters-grid/index.css";
+import type { Character } from "@/types/characters";
+import type { FC } from "react";
+export type CharactersGridProps = {
+  characters: Character[];
+};
+
+export const CharactersGrid: FC<CharactersGridProps> = ({ characters }) => {
+  if (characters.length === 0) {
+    return <div className="characters-grid-not-found">No characters found</div>;
+  }
+
+  return (
+    <div className="characters-grid">
+      {characters.map((character) => (
+        <CharacterCard {...character} />
+      ))}
+    </div>
+  );
+};
