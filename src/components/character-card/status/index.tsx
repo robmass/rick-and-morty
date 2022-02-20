aebaf7fd730e653bce7429bce11431e00a49bd99
robmass <robertomassimini@hotmail.com>
2022-02-20 15:31:27 +0100

    character card status component and stories

--- src/components/character-card/status/index.tsx
+++ src/components/character-card/status/index.tsx
@@ -2,11 +2,16 @@ import type { CharacterStatus } from "@/types/characters";
 import type { FC } from "react";
 import "./index.css";
 
-type CharacterCardStatusProps = {
+export type CharacterCardStatusProps = {
   status: CharacterStatus;
 };
 export const CharacterCardStatusIcon: FC<CharacterCardStatusProps> = ({
   status,
 }) => {
-  return <div className={`character-card-status-icon ${status}`} />;
+  return (
+    <div className={`character-card-status`}>
+      <span className={`character-card-status-icon ${status}`} />
+      {status}
+    </div>
+  );
 };
