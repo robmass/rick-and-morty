50b45fe345e84b3e2094188a4c6a85584c9fe46f
robmass <robertomassimini@hotmail.com>
2022-02-20 15:19:29 +0100

    card status component

--- src/components/character-card/status-info/index.tsx
+++ src/components/character-card/status-info/index.tsx
@@ -1,25 +1,12 @@
-import type { CharacterGender, CharacterStatus } from "@/types/characters";
+import type { CharacterStatus } from "@/types/characters";
 import type { FC } from "react";
 import "./index.css";
 
-type CharacterCardInfoProps = {
+type CharacterCardStatusProps = {
   status: CharacterStatus;
-  gender: CharacterGender;
-  species: string;
-  type: string;
 };
-export const CharacterCardInfo: FC<CharacterCardInfoProps> = ({
+export const CharacterCardStatusIcon: FC<CharacterCardStatusProps> = ({
   status,
-  gender,
-  species,
-  type,
 }) => {
-  const info = [status, gender, species].join(" - ");
-  return (
-    <div className="character-card-status">
-      <span className={`character-card-status-icon ${status}`} />
-      {info}
-      {type && ` - ${type}`}
-    </div>
-  );
+  return <div className={`character-card-status-icon ${status}`} />;
 };
