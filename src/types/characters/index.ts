8f32b9f2a1ddf8539377a409d398203c2f2496a8
robmass <robertomassimini@hotmail.com>
2022-02-20 23:55:24 +0100

    created CharacterApi type

--- src/types/characters/index.ts
+++ src/types/characters/index.ts
@@ -1,33 +1,8 @@
 import type { Episode } from "@/types/episode";
 import type { Location } from "@/types/location";
 
-type Origin = {
-  name: string;
-  url: string;
-};
-
-// type Location = {
-//   name: string;
-//   url: string;
-// };
-
 export type CharacterStatus = "Alive" | "Dead" | "Unknown";
 export type CharacterGender = "Female" | "Male" | "Genderless" | "Unknown";
-
-export type Character1 = {
-  id: number;
-  name: string;
-  status: CharacterStatus;
-  species: string;
-  type: string;
-  gender: CharacterGender;
-  origin: Origin;
-  location: Origin;
-  image: string;
-  episode: string[];
-  url: string;
-  created: string;
-};
 
 export type Character = {
   image: string;
