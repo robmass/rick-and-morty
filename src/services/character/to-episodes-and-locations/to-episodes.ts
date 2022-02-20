33cfcd9ec82e65f729d626bf81c3e649df4c738b
robmass <robertomassimini@hotmail.com>
2022-02-21 00:21:21 +0100

    removed log

--- src/services/character/to-episodes-and-locations/to-episodes.ts
+++ src/services/character/to-episodes-and-locations/to-episodes.ts
@@ -4,8 +4,6 @@ export const toEpisodes: (episodes: string[]) => number[] = (episodes) => {
   return episodes.reduce((ids: number[], episode) => {
     const id = extractIdFromUrl(episode);
     if (id !== -1) {
-      console.log(id);
-
       ids.push(id);
     }
     return ids;
