e282acd1d5fb5fa67a0e72c4d279f4b660c287b6
robmass <robertomassimini@hotmail.com>
2022-02-20 16:23:24 +0100

    created character fixture

--- src/stories/components/character-card/index.stories.tsx
+++ src/stories/components/character-card/index.stories.tsx
@@ -1,3 +1,4 @@
+import { CharacterFixture } from "@/stories/fixture/character";
 import type { Meta, Story } from "@storybook/react";
 import {
   CharacterCard,
@@ -13,50 +14,4 @@ const Template: Story<CharacterCardProps> = (props) => {
 };
 
 export const CharacterCardStory = Template.bind({});
-CharacterCardStory.args = {
-  name: "Rick Sanchez",
-  gender: "Male",
-  status: "Alive",
-  species: "Human",
-  image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-  type: "",
-  origin: {
-    name: "Earth",
-    type: "Planet",
-    dimension: "Dimension C-137",
-    amountOfResidents: 50000,
-  },
-  location: {
-    name: "Earth",
-    type: "Planet",
-    dimension: "Dimension C-137",
-    amountOfResidents: 50000,
-  },
-  episodes: [
-    {
-      name: "Close Rick-counters of the Rick Kind",
-      air_date: "April 7, 2014",
-      episode: "S01E10",
-    },
-    {
-      name: "The Ricklantis Mixup",
-      air_date: "September 10, 2017",
-      episode: "S03E07",
-    },
-    {
-      name: "The Ricklantis Mixup",
-      air_date: "September 10, 2017",
-      episode: "S03E07",
-    },
-    {
-      name: "The Ricklantis Mixup",
-      air_date: "September 10, 2017",
-      episode: "S03E07",
-    },
-    {
-      name: "The Ricklantis Mixup",
-      air_date: "September 10, 2017",
-      episode: "S03E07",
-    },
-  ],
-};
+CharacterCardStory.args = CharacterFixture;
