166f67d8dbff25a49e7d6d2693badab2d0936182
robmass <robertomassimini@hotmail.com>
2022-02-21 23:46:42 +0100

    remove unused code

--- src/services/parse-episode/index.ts
+++ src/services/parse-episode/index.ts
@@ -1,16 +1,7 @@
-import type { Episode } from "@/types/episode";
-import type { EpisodeApiSchema } from "@/types/server/episode-api";
+import type { Episode } from '@/types/episode';
+import type { EpisodeApiSchema } from '@/types/server/episode-api';
 
-export const parseEpisode: (apiEpisode: EpisodeApiSchema) => Episode = (
-  apiEpisode
-) => {
+export const parseEpisode: (apiEpisode: EpisodeApiSchema) => Episode = (apiEpisode) => {
   const { air_date, episode, name } = apiEpisode;
   return { air_date, episode, name };
-};
-
-export const findEpisodeGivenId: (
-  apiEpisodes: EpisodeApiSchema[],
-  id: number
-) => EpisodeApiSchema | undefined = (apiEpisodes, id) => {
-  return apiEpisodes.find((episode) => episode.id === id);
 };
