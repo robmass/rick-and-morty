1a1b2b94753e8ec802850d9821e0c8a3ac17ec0a
robmass <robertomassimini@hotmail.com>
2022-02-21 01:08:54 +0100

    check url in extractIdFromUrl

--- src/services/extract-id-from-url/index.ts
+++ src/services/extract-id-from-url/index.ts
@@ -1,4 +1,7 @@
 export const extractIdFromUrl: (url: string) => number = (url) => {
+  if (!url) {
+    return -1;
+  }
   const lastSlash = url.lastIndexOf("/") + 1;
   const id = url.slice(lastSlash);
   if (!id || isNaN(+id)) {
