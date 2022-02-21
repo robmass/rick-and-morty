3eddf7eb7eff730f7227b513956fa3207f0815d0
robmass <robertomassimini@hotmail.com>
2022-02-21 23:54:15 +0100

    renamed app

--- src/app.prova.tsx
+++ src/app.prova.tsx
@@ -1,7 +1,7 @@
 import { CharactersGrid } from '@/components/characters-grid';
 import { Pagination } from '@/components/pagination';
 import { useCharacters } from '@/hooks/character';
-import './App.css';
+import './app.css';
 
 function App() {
   const { characters, getCharacters, pageCount, actualPage } = useCharacters();
