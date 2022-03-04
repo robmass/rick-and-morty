96a63a4276e3956b7cd942f24462ccd51a5dbd05
robmass <robertomassimini@hotmail.com>
2022-03-04 17:13:12 +0100

    added styled components

--- src/components/character-card/index.tsx
+++ src/components/character-card/index.tsx
@@ -4,6 +4,7 @@ import type { CharacterGender, CharacterStatus } from '@/types/characters';
 import type { Episode } from '@/types/episode';
 import type { Location } from '@/types/location';
 import type { FC } from 'react';
+import styled from 'styled-components';
 import './index.css';
 
 export type CharacterCardProps = {
@@ -20,13 +21,19 @@ export type CharacterCardProps = {
 
 export const CharacterCard: FC<CharacterCardProps> = (props) => {
   const { image, name, gender, status, species, type } = props;
+  const stringa = `font-size: 1.5em;
+  text-align: center;
+  color: red;`;
+  const Name = styled.div`
+    ${stringa}
+  `;
   const info = [gender, species].join(' - ');
   return (
     <article className="character-card">
       <div className="character-card-side character-card-side-front">
         <img className="character-card-image" src={image} alt={name} />
         <div className="character-card-front-info">
-          <div className="character-card-name">{name}</div>
+          <Name>{name}</Name>
           <div className="character-card-status-info">
             <CharacterCardStatusIcon status={status} />
             {` - ${info}`}
